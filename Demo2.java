500936e083e05989b9cdd0e834104162afa4896e
Ganesh <149141995+ganesh-badar@users.noreply.github.com>
2025-11-24 20:45:07 +0530

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,45 @@
+package JDBC;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/voter?useSSL=false",
+                "root",
+                "YOUR_PASSWORD"
+            );
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Voter Id: ");
+            int id = sc.nextInt();
+
+            PreparedStatement pst = con.prepareStatement("select * from voters where voter_id = ?");
+            pst.setInt(1, id);
+
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                System.out.println(
+                    rs.getInt(1) + " " +
+                    rs.getString(2) + " " +
+                    rs.getInt(3)
+                );
+            } else {
+                System.out.println("Voter Not Found...");
+            }
+
+            rs.close();
+            sc.close();
+            con.close();
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
