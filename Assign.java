df36154ef51c8c1e1eea8ee83ac147a7d99a834d
Ganesh <149141995+ganesh-badar@users.noreply.github.com>
2025-11-25 14:49:21 +0530

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,45 @@
+package JDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class Assign {
+
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false","root","Ganesh@1351");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter id , First_Name , Last_Name , salary and joinin_Date(yyyy-mm-dd) ");
+			int id = sc.nextInt();
+			String fnm=sc.next();
+			String lnm = sc.next();
+			Float sal = sc.nextFloat();
+			String date = sc.next();
+			
+			
+			PreparedStatement pst = con.prepareStatement("insert into employees values (?,?,?,?,?)");
+			pst.setInt(1, id);
+			pst.setString(2, fnm);
+			pst.setString(3, lnm);
+			pst.setFloat(4, sal);
+			pst.setString(5, date);
+			pst.execute();
+			System.out.println("Record Inserted...");
+			con.close();
+			
+		
+		
+		
+		
+		
+		}
+		
+		catch(Exception Ex) {
+			System.out.println(Ex);
+		}
+	}
+
+}
